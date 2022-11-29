ea6117addf33f82d4920e023c3e824c397c041a7
Moises Navarro <2015313073@uteq.edu.mx>
2022-11-29 16:27:46 -0600

    Designn patterns update

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  static String baseUrl = 'http://74.208.25.75:2812';
-  //static String baseUrl = 'http://localhost:2812';
+  //static String baseUrl = 'http://74.208.25.75:2812';
+  static String baseUrl = 'http://localhost:2812';
   static String loginEndPoint = '/login';
   static String registrarUsuarioEndPoint = '/addUser';
   static String registrarAlumnoEndPoint = '/addAlumno';
